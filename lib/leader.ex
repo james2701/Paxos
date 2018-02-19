aadadf986c49b94fb581fee820278e4f1a2c5130
lydzfjzzw <jameszhangzhewei@outlook.com>
2018-02-19 15:18:06 +0000

    Add files via upload

--- lib/leader.ex
+++ lib/leader.ex
@@ -2,15 +2,11 @@
 # distributed algorithms, n.dulay 2 feb 18
 # coursework 2, paxos made moderately complex
 
-defmodule Leader do
+#defmodule Leader do
 
-def start config do
-  IO.puts ["          Starting server ", DAC.node_ip_addr()]
-  config   = Map.put config, :server_num, server_num  
+#def start config do
 
+#end # start
 
-  send paxos, { :config, replica, acceptor, leader }
-end # start
-
-end # Server
+#end # Server
 
