eb0efe4538e074527a2c06224c70ffd5fcc4f89c
lydzfjzzw <jameszhangzhewei@outlook.com>
2018-02-19 00:02:18 +0000

    Add files via upload

---
+++ lib/acceptor.ex
@@ -0,0 +1,36 @@
+
+# distributed algorithms, n.dulay 2 feb 18
+# coursework 2, paxos made moderately complex
+
+defmodule Acceptor do
+
+def start config do
+  bnum = 0
+  accepted = []
+  next config, bnum, accepted
+
+end # start
+
+def next config, bnum, accepted do
+  receive do 
+    {:p1a, lamda, b} -> 
+      newbnumber = cond do
+        b > bnum -> b
+        _ -> bnum
+      end
+      send lamda, {:p1b, self(), newbnum, accepted}
+    {:p2a, lamda, {b, s, c}} -> 
+      newaccepted = cond do
+        b = bnum -> accepted ++ {b, s, c}
+        _ -> accepted
+      end
+      send lamda, {:p2b, self(), bnum}
+      newbnum = bnum
+  end
+  next config, newbnum, newaccepted      
+
+end
+
+
+end # Acceptor
+
