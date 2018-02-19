878a2f5a7e23fa414d09a3e42748f6abc4b82616
lydzfjzzw <jameszhangzhewei@outlook.com>
2018-02-19 15:13:15 +0000

    Add files via upload

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -14,21 +14,20 @@ end # start
 def next config, bnum, accepted do
   receive do
     {:p1a, lamda, b} -> 
-      newbnumber = cond do
+      newbnum = cond do
         b > bnum -> b
         true -> bnum
       end
       send lamda, {:p1b, self(), newbnum, accepted}
-      newaccepted = accepted
+      next config, newbnum, accepted
     {:p2a, lamda, {b, s, c}} -> 
       newaccepted = cond do
         b == bnum -> accepted ++ {b, s, c}
         true -> accepted
       end
       send lamda, {:p2b, self(), bnum}
-      newbnum = bnum
+      next config, bnum, newaccepted
   end
-  next config, newbnum, newaccepted
 
 end
 
